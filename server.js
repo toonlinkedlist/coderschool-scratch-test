1f94954ae9946e81554160d61acf3d152e3590da
SETH BLUMER <sethblumer1@gmail.com>
2022-09-27 14:08:35 -0400

    "test change"

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
+// test change
+
 // app.listen(process.env.PORT, () => {
 //   console.log(`Server running on port ${process.env.PORT}`);
 // });
