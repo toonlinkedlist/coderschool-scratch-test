707fff5a604b8aa2ad0c4b2878dbfa811b2e3a95
SETH BLUMER <sethblumer1@gmail.com>
2022-09-27 13:57:17 -0400

    "heroku changes"

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 // Import routes
 const scratchRoute = require("./routes/scratch");
@@ -9,6 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use("/api/scratch", scratchRoute);
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
