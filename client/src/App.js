d9ea6e4f0efbeeadf88b559747972348099c04a6
sethblumer1 <86381325+sethblumer1@users.noreply.github.com>
2022-12-12 17:05:28 -0500

    "removed <App> comments"

--- client/src/App.js
+++ client/src/App.js
@@ -5,31 +5,6 @@ import Layout from "./Layout";
 
 function App() {
   return <Layout />;
-  // const [fullName, setFullName] = useState("");
-
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
-  //   axios.post("/api/scratch/create", { fullName }).then((res) => {
-  //     console.log(res);
-  //   });
-  // };
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <p>Testing Scratch template creation</p>
-  //       <input
-  //         type="text"
-  //         value={fullName}
-  //         onChange={(e) => setFullName(e.target.value)}
-  //         placeholder="Enter full name"
-  //         style={{ marginBottom: "10px" }}
-  //       ></input>
-  //       <button className="btn" onClick={(e) => onSubmit(e)}>
-  //         Test
-  //       </button>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
